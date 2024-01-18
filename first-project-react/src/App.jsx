cfba92c7b988eeffcc848542d86834d330b07342
sabbat10 <sabbatlumpatshia99@gmail.com>
2024-01-18 05:15:34 +0100

    add images to my project

--- first-project-react/src/App.jsx
+++ first-project-react/src/App.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { Children } from "react";
 import Superheros from "./components/Superheros/Superheros";
 import Superhero from "./components/Superheros/Superhero/Superhero";
 import "./App.css";
+import iromMan from "../public/iron man.jpg";
+import loki from "../public/loki.jpeg";
+import capitaineAmerica from "../public/captain-america.webp";
 
 export default function App() {
   return (
@@ -13,18 +16,21 @@ export default function App() {
           nom="Iron Man"
           description="Alors qu'il fait l'essai d'une arme de son invention en Afghanistan, le milliardaire Tony Stark est capturé par des insurgés qui le forcent à travailler pour eux. Mais à leur insu, le scientifique crée pour lui-même une armure superpuissante au moyen de laquelle il s'évade et rentre aux États-Unis."
           films={["Iron Man 1", "Iron Man 2", "Iron Man 3"]}
+          // photo={iromMan}
         />
 
         {/* Superhero numéro 2 */}
         <Superhero
           nom="Loki"
           description="Dans une réalité alternative, Loki s'est échappé avec le Tesseract. Après son évasion, il est emmené auprès du Tribunal des Variations Anachroniques, un organisme qui agit pour arrêter toute personne qui tenterait d'altérer l'éternel flux temporel."
+          photo={loki}
         />
 
         {/* Superhero numéro 3 */}
         <Superhero
           nom="Capitain America"
           description="Le film raconte l'histoire de Steve Rogers, un jeune homme frêle de Brooklyn transformé en un super-soldat nommé Captain America durant la Seconde Guerre .."
+          photo={capitaineAmerica}
         />
       </Superheros>
     </div>
--- first-project-react/src/components/Superheros/Superhero/Superhero.jsx
+++ first-project-react/src/components/Superheros/Superhero/Superhero.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "./Superhero.css";
+// import profil from ;
 
 export default function Superhero({
   nom,
   description,
   films = ["Aucun film"],
+  photo = "../../../../public/profil.jpeg",
   ...props
 }) {
   return (
     <div className="superhero">
+      <img src={photo} alt={`${nom} photo`} />
       <h2>{nom}</h2>
       <p>{description}</p>
       <ul>
